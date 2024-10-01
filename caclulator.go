b947d91e010dccd05b47943755a243f44945164f
Rey-off <raevskiy.official@mail.ru>
2024-10-01 13:44:51 +0300

    Update caclulator.go

--- caclulator.go
+++ caclulator.go
@@ -8,12 +8,16 @@ import (
 
 var romanNumerals = map[string]int{  
 	"I":  1, "II": 2, "III": 3, "IV": 4, "V": 5,  
-	"VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,  
+	"VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
+	"XX": 20, "XXX": 30, "XL": 40, "L": 50,
+	"LX": 60, "LXX": 70, "LXXX": 80, "XC": 90, "C": 100,
 }  
 
 var arabicNumerals = map[int]string{  
-	1: "I", 2: "II", 3: "III", 4: "IV", 5: "V",   
-	6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X",  
+	1: "I", 2: "II", 3: "III", 4: "IV", 5: "V",
+	6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X",
+        20: "XX", 30: "XXX", 40: "XL", 50: "L",
+        60: "LX", 70: "LXX", 80: "LXXX", 90: "XC", 100: "C",
 }  
 
 func main() {  
